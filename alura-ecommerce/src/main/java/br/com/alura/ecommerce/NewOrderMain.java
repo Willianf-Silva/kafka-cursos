126c27185758715a529fd76c3b9d732bcd309a73
WillianF-Silva <willian.ferreira.da.silva@gmail.com>
2023-12-04 22:38:04 -0300

    Creating kafka producer simple

--- alura-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
+++ alura-ecommerce/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -1,8 +1,39 @@
 package br.com.alura.ecommerce;
 
-public class NewOrderMain {
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    final static String CONST_SERVIDOR_KAFKA = "127.0.0.1:9092";
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        var producer = new KafkaProducer<String, String>(properties());
+        var value = "123123,654987,789456123";
+        var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
+        producer.send(record, (data, ex) -> {
+            if (ex != null){
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println(
+                    "sucesso enviando " + data.topic() +
+                            "::: partition "+ data.partition() +
+                            "/ offset " + data.offset() +
+                            "/ timestamp " + data.timestamp()
+            );
+        }).get();
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CONST_SERVIDOR_KAFKA);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
 }
